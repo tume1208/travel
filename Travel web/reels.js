7b238ec464b05459d2dfdb47e9cddcad316c4662
tume1208 <tumenulzii1203@gmail.com>
2025-01-31 21:11:05 +0800

    Update reels.js

--- Travel web/reels.js
+++ Travel web/reels.js
@@ -17,7 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (post.post_video) {
                     fullScreenVideoElement.src = post.post_video;
                     fullScreenVideoElement.load();
-                    document.addEventListener('click', () => {
+                    document.addEventListener('click', (event) => {
+                        event.preventDefault(); // Prevent default controls
                         fullScreenVideoElement.play().catch(error => {
                             console.error('Error playing video:', error);
                         });
